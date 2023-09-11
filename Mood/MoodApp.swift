a797b0d13aa851e1e76d407809e7890ef8027f3d
nate-leake <nateleakenasa@gmail.com>
2023-09-11 17:33:23 -0500

    Initial Commit

---
+++ Mood/MoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodApp.swift
+//  Mood
+//
+//  Created by Nate Leake on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
