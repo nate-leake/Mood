a98d8754ba7cdf4ad5f334aeafbad88a69283f9f
nate-leake <nateleakenasa@gmail.com>
2023-09-16 18:34:24 -0500

    Added logout button to ProfileView

--- Mood/Core/Profile/ProfileView.swift
+++ Mood/Core/Profile/ProfileView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    func logout(){
+        print("log out user")
+    }
+    
     var body: some View {
         NavigationStack {
             Text("This is your profile")
+            
+            Button {
+                logout()
+            } label: {
+                Text("log out")
+            }
             
             .navigationTitle("Profile")
             .navigationBarTitleDisplayMode(.inline)
