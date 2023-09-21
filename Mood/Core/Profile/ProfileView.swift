7da0bdf4101a48dcacb9e8f9acbe0901d3df7702
nate-leake <nateleakenasa@gmail.com>
2023-09-21 15:02:11 -0500

    Added new UI elements

--- Mood/Core/Profile/ProfileView.swift
+++ Mood/Core/Profile/ProfileView.swift
@@ -11,13 +11,63 @@ struct ProfileView: View {
     
     var body: some View {
         NavigationStack {
-            Text("This is your profile")
-            
-            Button {
-                AuthService.shared.signout()
-            } label: {
-                Text("log out")
+            VStack{
+                VStack{
+                    Text("37")
+                        .font(.title)
+                    Text("entries")
+                }
+                .foregroundStyle(.white)
+                .background(
+                    RoundedRectangle(cornerRadius: 8.0)
+                        .fill(.appGreen)
+                        .frame(width: 80, height: 80)
+                )
+                .padding(.top, 20)
+                .padding(.bottom, 10)
+                
+                List{
+                    Section{
+                        HStack{
+                            Text("name")
+                            Spacer()
+                            Text("Bob")
+                                .font(.footnote)
+                                .foregroundStyle(.gray)
+                        }
+                        HStack{
+                            Text("email")
+                            Spacer()
+                            Text(verbatim: "bob@gmail.com")
+                                .font(.footnote)
+                                .foregroundStyle(.gray)
+                        }
+                        HStack{
+                            Text("birthday")
+                            Spacer()
+                            Text("August 12, 3019")
+                                .font(.footnote)
+                                .foregroundStyle(.gray)
+                        }
+                        
+                    }
+                    .listRowBackground(Color(.appPurple).opacity(0.1))
+                    
+                    Button {
+                        AuthService.shared.signout()
+                    } label: {
+                        HStack{
+                            Spacer()
+                            Text("log out")
+                                .foregroundStyle(.appRed)
+                            Spacer()
+                        }
+                    }
+                    .listRowBackground(Color(.appPurple).opacity(0.1))
+                }
+                .scrollContentBackground(.hidden)
             }
+            
             
             .navigationTitle("Profile")
             .navigationBarTitleDisplayMode(.inline)
