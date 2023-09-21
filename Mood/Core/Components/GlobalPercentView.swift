432cc213f5f17e5bf1af5db7b0f045d158949e62
nate-leake <nateleakenasa@gmail.com>
2023-09-21 17:12:44 -0500

    Built out these views a bit more

--- Mood/Core/Components/GlobalPercentView.swift
+++ Mood/Core/Components/GlobalPercentView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct GlobalPercentView: View {
+    var context: String
+    var color: Color
+    var percent: Int
+    
+    
+    
+    private var gradient: LinearGradient
+    
+    init(context: String, color: Color, percent: Int) {
+        self.context = context
+        self.color = color
+        self.percent = percent
+        
+        let stops = [
+            Gradient.Stop(color: color, location: CGFloat(percent)/100),
+            Gradient.Stop(color: color.opacity(0.5), location: CGFloat(percent)/100)
+        ]
+        
+        self.gradient = LinearGradient(stops: stops, startPoint: .leading, endPoint: .trailing)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Text(context)
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            ZStack{
+                RoundedRectangle(cornerRadius: 8.0)
+                    .fill(gradient)
+                    .frame(height: 70)
+                    .padding(.horizontal, 20)
+                
+                Text("\(percent)%")
+                    .foregroundStyle(.black.opacity(0.7))
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
+        }
     }
 }
 
 #Preview {
-    GlobalPercentView()
+    GlobalPercentView(context: "family", color: .anger, percent: 50)
 }
--- Mood/Core/GlobalMood/GlobalMoodView.swift
+++ Mood/Core/GlobalMood/GlobalMoodView.swift
@@ -10,7 +10,25 @@ import SwiftUI
 struct GlobalMoodView: View {
     var body: some View {
         NavigationStack {
-            Text("This is the global mood data")
+            ScrollView{
+                Text("this is how people feel today")
+                Spacer()
+                
+                VStack{
+                    
+                    GlobalPercentView(context: "family", color: .happiness, percent: 92)
+                    GlobalPercentView(context: "health", color: .anger, percent: 85)
+                    GlobalPercentView(context: "identity", color: .neutrality, percent: 64)
+                    GlobalPercentView(context: "money", color: .fearful, percent: 51)
+                    GlobalPercentView(context: "politics", color: .sadness, percent: 31)
+                    GlobalPercentView(context: "weather", color: .neutrality, percent: 29)
+                    GlobalPercentView(context: "work", color: .happiness, percent: 14)
+                    
+                }
+                Spacer()
+                
+                    
+            }
             
             .navigationTitle("Global mood")
             .navigationBarTitleDisplayMode(.inline)
