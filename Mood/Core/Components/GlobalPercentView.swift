a237dbc7ca472e08ed08b8f306263940058484a9
nate-leake <nateleakenasa@gmail.com>
2023-09-21 17:12:01 -0500

    Added this sub view to support GlobalMoodView

---
+++ Mood/Core/Components/GlobalPercentView.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalPercentView.swift
+//  Mood
+//
+//  Created by Nate Leake on 9/21/23.
+//
+
+import SwiftUI
+
+struct GlobalPercentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlobalPercentView()
+}
