b19302960241e43db6df72e2711336ad2980c42a
nate-leake <nateleakenasa@gmail.com>
2023-10-01 21:24:29 -0500

    Added password validation

--- Mood/Core/Authentication/View/CreatePasswordView.swift
+++ Mood/Core/Authentication/View/CreatePasswordView.swift
@@ -11,19 +11,55 @@ struct CreatePasswordView: View {
     @Environment(\.dismiss) var dismiss
     @EnvironmentObject var viewModel: RegistrationViewModel
     
+    @State private var isFormValid: Bool = false
+    
+    func isValidPassword() -> Bool {
+        
+//        ^                         Start anchor
+//        (?=.*[A-Z])        Ensure string has one uppercase letters.
+//        (?=.*[!@#$&*])            Ensure string has one special case letter.
+//        (?=.*[0-9])        Ensure string has one digits.
+//        (?=.*[a-z].*[a-z].*[a-z]) Ensure string has three lowercase letters.
+//        .{8}                      Ensure string is of length 8.
+//        $                         End anchor.
+        
+        let psswdRegEx = "^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$"
+
+        let psswdPred = NSPredicate(format:"SELF MATCHES %@", psswdRegEx)
+        return psswdPred.evaluate(with: viewModel.passwd)
+    }
+    
     var body: some View {
         ZStack{
             Color.appPurple
                 .ignoresSafeArea()
             
             VStack{
-                
                 Text("create a password")
                     .font(.title)
                     .foregroundStyle(.white)
                     .fontWeight(.bold)
-                Text("it should be at least 6 charachters")
+                Text("it should have")
                     .foregroundStyle(.white)
+                
+                HStack{
+                    Spacer()
+                    Text("\u{2022} at least 8 characters")
+                        .foregroundStyle(.white)
+                    Spacer()
+                    Text("\u{2022} one number")
+                        .foregroundStyle(.white)
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Text("\u{2022} one special character")
+                        .foregroundStyle(.white)
+                    Spacer()
+                    Text("\u{2022} one upper case letter")
+                        .foregroundStyle(.white)
+                    Spacer()
+                }
                 
                 Rectangle()
                     .frame(width: 300, height: 0.5)
@@ -33,6 +69,9 @@ struct CreatePasswordView: View {
                     .textInputAutocapitalization(.never)
                     .modifier(TextFieldModifier())
                     .padding(.vertical)
+                    .onChange(of: viewModel.passwd) {
+                        self.isFormValid = isValidPassword()
+                    }
                 
                 NavigationLink{
                     AddNameView()
@@ -45,7 +84,13 @@ struct CreatePasswordView: View {
                         .foregroundStyle(.appPurple)
                         .background(.white)
                         .clipShape(RoundedRectangle(cornerRadius: 8))
-                }
+                    
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundStyle(isFormValid ? .black.opacity(0.0) : .black.opacity(0.2))
+                                .animation(.easeInOut(duration: 0.25), value: isFormValid)
+                        }
+                }.disabled(!isFormValid)
                 
                 Spacer()
             }
