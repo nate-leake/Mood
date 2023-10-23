78934f4506a2ee8f3e95a977bffd99c8dc323777
nate-leake <nateleakenasa@gmail.com>
2023-10-23 14:33:57 -0500

    Initial commit

---
+++ Mood/Core/GlobalMood/GlobalMoodLockedView.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalMoodLockedView.swift
+//  Mood
+//
+//  Created by Nate Leake on 10/23/23.
+//
+
+import SwiftUI
+
+struct GlobalMoodLockedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlobalMoodLockedView()
+}
---
+++ Mood/Core/GlobalMood/GlobalMoodViewWrapper.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalMoodViewWrapper.swift
+//  Mood
+//
+//  Created by Nate Leake on 10/23/23.
+//
+
+import SwiftUI
+
+struct GlobalMoodViewWrapper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlobalMoodViewWrapper()
+}
