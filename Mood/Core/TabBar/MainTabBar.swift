4a2a914da3afba66ff1f6e4cc8bead0daf71e296
nate-leake <nateleakenasa@gmail.com>
2024-08-18 09:59:37 -0500

    Updated default tab selection

--- Mood/Core/TabBar/MainTabBar.swift
+++ Mood/Core/TabBar/MainTabBar.swift
@@ -8,31 +8,36 @@
 import SwiftUI
 
 struct MainTabBar: View {
+    @State private var selection = 2
     @EnvironmentObject var dailyDataService: DailyDataService
     let user: User
     
     var body: some View {
-        TabView{
+        TabView(selection:$selection){
             GlobalMoodViewWrapper()
                 .environmentObject(dailyDataService)
                 .tabItem {
                     Image(systemName: "globe.americas")
                 }
+                .tag(1)
             
             YourMoodView()
                 .environmentObject(dailyDataService)
                 .tabItem {
                     Image(systemName: "brain")
                 }
+                .tag(2)
             
             ProfileView(user: user)
                 .tabItem {
                     Image(systemName: "person")
                 }
+                .tag(3)
         }
     }
 }
 
 #Preview {
     MainTabBar(user: User.MOCK_USERS[0])
+        .environmentObject(DailyDataService())
 }
