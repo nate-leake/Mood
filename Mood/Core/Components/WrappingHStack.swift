ee8bddd17efdf4419896b4b851a416d0d8d72f23
nate-leake <nateleakenasa@gmail.com>
2024-09-10 12:08:01 -0500

    Updated happiness color

---
+++ Mood/Core/Components/WrappingHStack.swift
@@ -0,0 +1,18 @@
+//
+//  WrappingHStack.swift
+//  Mood
+//
+//  Created by Nate Leake on 9/8/24.
+//
+
+import SwiftUI
+
+struct WrappingHStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WrappingHStack()
+}
