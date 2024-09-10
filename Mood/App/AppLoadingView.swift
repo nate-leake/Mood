25355ee3294ac8dc80fb3255e607b98e0efacf58
nate-leake <nateleakenasa@gmail.com>
2024-09-10 12:08:52 -0500

    Added Launch and Loading screen

--- Mood/App/AppLoadingView.swift
+++ Mood/App/AppLoadingView.swift
@@ -8,11 +8,69 @@
 import SwiftUI
 
 struct AppLoadingView: View {
+    @Binding var isShowingTabView: Bool
+    @StateObject var dailyDataService: DailyDataService = DailyDataService.shared
+    @State var isAnimating: Bool = false
+    @State var scale: CGFloat = 1.0
+    
+    
+    // Starts the scaling animation
+    private func startAnimation() {
+        isAnimating = true
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            if !isAnimating {
+                timer.invalidate() // Stop the timer when animation should end
+            } else {
+                withAnimation(.snappy(duration: 0.25)) {
+                    scale = 1.25
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) { // Short time at scale 1.5
+                    withAnimation(.snappy(duration: 0.25).delay(0)) { // Longer time returning to 1.0
+                        scale = 1.0
+                    }
+                }
+            }
+        }
+    }
+    
+    // Stops the scaling animation
+    private func stopAnimation() {
+        isAnimating = false
+        scale = 1.0 // Reset to original scale
+    }
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.splashScreen.ignoresSafeArea()
+            
+            HStack{
+                Image("SplashScreenImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250, height: 130)
+                    .scaleEffect(scale)
+                //                    .animation(isAnimating ? asymmetricalAnimation() : .default, value: scale)
+                    .onAppear {
+                        startAnimation()
+                    }
+                    .onChange(of: dailyDataService.appIsReady) { oldValue, newValue in
+                        if newValue {
+                            stopAnimation()
+                            withAnimation(.easeInOut){
+                                isShowingTabView = true
+                            }
+                        }
+                    }
+                
+            }
+        }
+        
     }
 }
 
 #Preview {
-    AppLoadingView()
+    @Previewable @State var isShowingTabView = false
+    AppLoadingView(isShowingTabView: $isShowingTabView)
 }
