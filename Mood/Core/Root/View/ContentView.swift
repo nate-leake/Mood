34cc21441eaa7cdd5cb0d43f698af2792ab98986
nate-leake <nateleakenasa@gmail.com>
2024-09-10 12:09:39 -0500

    Adds support for the launch and loading screen

--- Mood/Core/Root/View/ContentView.swift
+++ Mood/Core/Root/View/ContentView.swift
@@ -11,15 +11,20 @@ struct ContentView: View {
     @StateObject var viewModel = ContentViewModel()
     @StateObject var registrationViewModel = RegistrationViewModel()
     @StateObject var dailyDataService: DailyDataService = DailyDataService.shared
+    @State var displayingTabView: Bool = false
     
     var body: some View {
         Group {
-            if viewModel.userSession == nil {
-                WelcomeView()
-                    .environmentObject(registrationViewModel)
-            } else if let currentUser = viewModel.currentUser {
-                MainTabBar(user: currentUser)
-                    .environmentObject(dailyDataService)
+            if displayingTabView {
+                if viewModel.userSession == nil {
+                    WelcomeView()
+                        .environmentObject(registrationViewModel)
+                } else if let currentUser = viewModel.currentUser {
+                    MainTabBar(user: currentUser)
+                        .environmentObject(dailyDataService)
+                }
+            } else {
+                AppLoadingView(isShowingTabView: $displayingTabView)
             }
         }
     }
