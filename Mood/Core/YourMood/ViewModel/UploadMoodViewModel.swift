1c4679f2e29bb652f35d644af09558a4e047261c
nate-leake <nateleakenasa@gmail.com>
2024-09-20 16:26:16 -0500

    Added property to track log success state

--- Mood/Core/YourMood/ViewModel/UploadMoodViewModel.swift
+++ Mood/Core/YourMood/ViewModel/UploadMoodViewModel.swift
@@ -12,6 +12,7 @@ import Firebase
 class UploadMoodViewModel: ObservableObject {
     private var dailyData: DailyData = DailyData(date: Date(), timeZoneOffset: TimeZone.current.secondsFromGMT(for: Date()), pairs: [])
     @Published var pairs: [ContextEmotionPair] = []
+    @Published var isUploaded: Bool = false
     
     func containsPair(withContext contextName: String) -> Bool {
         return pairs.contains { $0.contextName == contextName }
@@ -24,17 +25,14 @@ class UploadMoodViewModel: ObservableObject {
     
     @MainActor
     func uploadMoodPost() async throws -> Bool {
-        for pair in pairs {
-            print("pair to upload: \(pair.contextName) with emotions \(pair.emotions) and weight \(pair.weight.rawValue)")
-            self.dailyData.addPair(pair: pair)
-        }
-        
         let uploaded = try await DailyDataService.shared.uploadMood(dailyData: self.dailyData)
         if uploaded {
             DailyDataService.shared.userHasLoggedToday = true
+            isUploaded = true
             return true
         } else {
             print("Something went wrong uploading mood post!")
+            isUploaded = false
             return false
         }
     }
