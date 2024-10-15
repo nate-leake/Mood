c70f2f4a55bf9417262e924d66ac56deddb0aa7e
nate-leake <nateleakenasa@gmail.com>
2024-10-15 08:37:38 -0500

    Created Setting and Privacy screen

--- Mood/Core/Profile/Settings and Privacy/SettingsAndPrivacyView.swift
+++ Mood/Core/Profile/Settings and Privacy/SettingsAndPrivacyView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct SettingsAndPrivacyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("")
+        }
+        .navigationTitle("settings and privacy")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
