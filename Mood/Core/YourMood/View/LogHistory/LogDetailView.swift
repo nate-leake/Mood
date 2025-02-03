72773c6c9195e2599ae18c33fd887fbec603e47e
nate-leake <nateleakenasa@gmail.com>
2025-02-02 19:59:52 -0600

    Initial commit

--- Mood/Core/YourMood/View/LogHistory/LogDetailView.swift
+++ Mood/Core/YourMood/View/LogHistory/LogDetailView.swift
@@ -7,12 +7,81 @@
 
 import SwiftUI
 
-struct LogDetailView: View {
+struct LogItemView: View {
+    var post: UnsecureMoodPost
+    var context: UnsecureContext
+    
+    @State var moodLogContainers: [MoodLogContainer] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Section{
+            HStack {
+                Image(systemName: context.iconName)
+//                    .opacity(0.8)
+                Text("\(context.name)")
+                    .fontWeight(.bold)
+//                    .opacity(0.8)
+            }
+            .foregroundStyle(context.color.isLight() ? .black : .white)
+            .modifier(ListRowBackgroundModifer(foregroundColor: context.color.opacity(0.85)))
+            
+            VStack {
+                ForEach(moodLogContainers, id: \.moodName) {log in
+                    HStack {
+                            
+                        Text("\(log.moodName)")
+                            .padding(10)
+                            .background(Color(log.moodName))
+                            .foregroundStyle(Color(log.moodName).isLight() ? .black : .white)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            
+                        Text("\(log.emotions.joined(separator: ", "))")
+                            
+                        
+                        Spacer()
+                    }
+                }
+                
+            }
+            .modifier(ListRowBackgroundModifer())
+        }
+        .onAppear{
+            moodLogContainers = []
+            print(post)
+            for pair in post.data { // loop through each ContextEmotionPair in the post (there can be multiple CEPs for one context)
+//                print(pair.contextId, context.id)
+                if pair.contextId == context.id { // check if the CEP matches the current context
+                    moodLogContainers = pair.moodContainers
+                }
+            }
+            print(moodLogContainers)
+        }
+    }
+}
+
+struct LogDetailView: View {
+    var post: UnsecureMoodPost
+    
+    var body: some View {
+        
+        List{
+            ForEach(post.data, id: \.contextId) { data in
+                if let context = UnsecureContext.getContext(from: data.contextId){
+                    LogItemView(post: post, context: context)
+                } else {
+                    Text("nil context\n\(data.contextId)")
+                }
+            }
+        }
+        .scrollContentBackground(.hidden)
+        .navigationTitle("\(ShortDate().string(from: post.timestamp))")
+        .navigationBarTitleDisplayMode(.inline)
+        
     }
 }
 
 #Preview {
-    LogDetailView()
+    NavigationStack{
+        LogDetailView(post: UnsecureMoodPost.MOCK_DATA[1])
+    }
 }
