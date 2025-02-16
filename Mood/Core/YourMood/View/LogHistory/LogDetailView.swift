cd639273dbe1ffc67f68d50dc08ba9edcfa3e00c
nate-leake <nateleakenasa@gmail.com>
2025-02-16 09:51:15 -0600

    Changed how data is managed. Updated UI. Added non-functional delete button

--- Mood/Core/YourMood/View/LogHistory/LogDetailView.swift
+++ Mood/Core/YourMood/View/LogHistory/LogDetailView.swift
@@ -8,70 +8,99 @@
 import SwiftUI
 
 struct LogItemView: View {
-    var post: UnsecureMoodPost
+    var contextLogContainer: ContextLogContainer
     var context: UnsecureContext
     
-    @State var moodLogContainers: [MoodLogContainer] = []
+    init(contextLogContainer: ContextLogContainer) {
+        self.contextLogContainer = contextLogContainer
+        self.context = UnsecureContext.getContext(from: contextLogContainer.contextId)!
+    }
     
     var body: some View {
         Section{
             HStack {
                 Image(systemName: context.iconName)
-//                    .opacity(0.8)
+                //                    .opacity(0.8)
                 Text("\(context.name)")
                     .fontWeight(.bold)
-//                    .opacity(0.8)
+                //                    .opacity(0.8)
             }
             .foregroundStyle(context.color.isLight() ? .black : .white)
             .modifier(ListRowBackgroundModifer(foregroundColor: context.color.opacity(0.85)))
             
             VStack {
-                ForEach(moodLogContainers, id: \.moodName) {log in
-                    HStack {
-                            
+                ForEach(contextLogContainer.moodContainers, id: \.moodName) {log in
+                    HStack(alignment: .top) {
                         Text("\(log.moodName)")
-                            .padding(10)
+                            .frame(width:100)
+                            .padding(7)
                             .background(Color(log.moodName))
                             .foregroundStyle(Color(log.moodName).isLight() ? .black : .white)
                             .clipShape(RoundedRectangle(cornerRadius: 8))
-                            
-                        Text("\(log.emotions.joined(separator: ", "))")
-                            
                         
                         Spacer()
+                        
+                        VStack(alignment: .leading) {
+                            ForEach(log.emotions, id:\.self) { text in
+                                Text(text)
+                                    .frame(maxWidth: .infinity)
+                                    .padding(7)
+                                    .background(Color(log.moodName).opacity(0.5))
+                                //                                    .foregroundStyle(Color(log.moodName).opacity(0.5).isLight() ? .black : .white)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            }
+                        }
+                        .padding(.leading, 5)
                     }
+                    .padding(.vertical, 7)
                 }
                 
             }
             .modifier(ListRowBackgroundModifer())
-        }
-        .onAppear{
-            moodLogContainers = []
-            print(post)
-            for pair in post.data { // loop through each ContextEmotionPair in the post (there can be multiple CEPs for one context)
-//                print(pair.contextId, context.id)
-                if pair.contextId == context.id { // check if the CEP matches the current context
-                    moodLogContainers = pair.moodContainers
-                }
-            }
-            print(moodLogContainers)
         }
     }
 }
 
 struct LogDetailView: View {
     var post: UnsecureMoodPost
+    @State var isShowingConfirmation: Bool = false
     
     var body: some View {
         
         List{
-            ForEach(post.data, id: \.contextId) { data in
-                if let context = UnsecureContext.getContext(from: data.contextId){
-                    LogItemView(post: post, context: context)
-                } else {
-                    Text("nil context\n\(data.contextId)")
+            ForEach(post.contextLogContainers, id: \.contextId) { container in
+                LogItemView(contextLogContainer: container)
+            }
+            
+            Button{
+                isShowingConfirmation = true
+            } label: {
+                HStack{
+                    Spacer()
+                    Image(systemName: "trash")
+                    Text("delete log")
+                    Spacer()
                 }
             }
+            .bold()
+            .foregroundStyle(.white)
+            .modifier(ListRowBackgroundModifer(foregroundColor: .appRed))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .alert(
+                "this action will permenantly erase this entry. this action cannot be undone.", isPresented: $isShowingConfirmation
+            ) {
+                Button("cancel", role: .cancel) {
+                }
+
+                Button("delete", role: .destructive) {
+                    Task {
+                        print("delete day")
+                    }
+                }
+
+            }
+            
+            
         }
         .scrollContentBackground(.hidden)
         .navigationTitle("\(ShortDate().string(from: post.timestamp))")
