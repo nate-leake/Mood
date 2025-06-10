95d5883a7f264eb7f14a167240f4ba5cf591a648
nate-leake <nateleakenasa@gmail.com>
2025-06-10 16:19:07 -0500

    Added new comment warning for testing unlock feature

--- Mood/Core/Authentication/Service/AuthService.swift
+++ Mood/Core/Authentication/Service/AuthService.swift
@@ -38,6 +38,10 @@ class AuthService: Stateable, ObservableObject {
     init(){
 //        self.signout()
 //        #warning("AuthService will sign out on every launch.")
+        
+//        self.isUnlocked = true
+//        #warning("AuthService Security Warning: is unlocked at every launch.")
+        
         AppState.shared.addContributor(adding: self)
         Task {try await loadUserData()}
     }
