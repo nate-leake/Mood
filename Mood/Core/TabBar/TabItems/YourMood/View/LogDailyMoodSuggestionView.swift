65f526689dfffe6cfb4e503ca5b5d075cd5c2b22
nate-leake <nateleakenasa@gmail.com>
2025-06-10 21:56:59 -0500

    Fixed navLink to only trigger on press of log pill

--- Mood/Core/TabBar/TabItems/YourMood/View/LogDailyMoodSuggestionView.swift
+++ Mood/Core/TabBar/TabItems/YourMood/View/LogDailyMoodSuggestionView.swift
@@ -12,25 +12,28 @@ struct LogDailyMoodSuggestionView: View {
     
     var body: some View {
         VStack(alignment: .leading){
-            
+            HStack{
+                Text("logging consistently can help build awareness")
+                    .font(.system(.callout))
+                    .multilineTextAlignment(.leading)
+                    .foregroundStyle(.appBlack)
+                
+                Spacer()
+                
                 NavigationLink{
                     ContextTileView()
                 } label: {
-                    HStack{
-                        Text("logging consistently can help build awareness")
-                            .font(.system(.callout))
-                            .multilineTextAlignment(.leading)
-                            .foregroundStyle(.appBlack)
-                        
-                        Spacer()
-                        
-                    Text("log")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                        .padding(EdgeInsets(top: 7, leading: 15, bottom: 7, trailing: 15))
-                        .foregroundStyle(.white)
-                        .background(.appPurple)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    HStack {
+                        Image(systemName: "plus")
+                            
+                        Text("log")
+                    }
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .padding(EdgeInsets(top: 7, leading: 11, bottom: 7, trailing: 13))
+                    .foregroundStyle(.white)
+                    .background(.appPurple)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
             }
         }
